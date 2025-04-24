064cf4fbc7d55d256e236881acf56e48e89f3755
freyder <freyder@hotmail.com>
2025-04-24 10:33:03 -0500

    Update OrderStorageService.java

--- src/main/java/com/foo/worker/service/OrderStorageService.java
+++ src/main/java/com/foo/worker/service/OrderStorageService.java
@@ -4,7 +4,10 @@ import com.foo.worker.models.Order;
 
 import reactor.core.publisher.Mono;
 
+/**
+ * OrderStorageService: Interface responsible for persisting enriched Order objects
+ * into MongoDB using a reactive approach.
+ */
 public interface OrderStorageService {
     Mono<Order> saveOrder(Order order);
-
 }
