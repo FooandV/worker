62dc7793f3df9e9da2040bce99e332869c1e91db
freyder <freyder@hotmail.com>
2025-04-24 10:17:25 -0500

    Update RedisConfig.java

--- src/main/java/com/foo/worker/config/RedisConfig.java
+++ src/main/java/com/foo/worker/config/RedisConfig.java
@@ -6,12 +6,20 @@ import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
+/**
+ * Configuration class that sets up a reactive RedisTemplate 
+ * for non-blocking Redis operations using String keys and values.
+ */
 @Configuration
 public class RedisConfig {
 
+    /**
+     * Defines a reactive RedisTemplate using String serialization.
+     * This is used to interact with Redis in a reactive, non-blocking way.
+     */
     @Bean
     public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
         return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
     }
-    
+
 }
