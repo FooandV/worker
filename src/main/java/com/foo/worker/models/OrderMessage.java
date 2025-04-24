d81eb62b84ff060a631c6cef6b712c528b40cd28
freyder <freyder@hotmail.com>
2025-04-24 10:21:57 -0500

    Update OrderMessage.java

--- src/main/java/com/foo/worker/models/OrderMessage.java
+++ src/main/java/com/foo/worker/models/OrderMessage.java
@@ -1,14 +1,15 @@
 package com.foo.worker.models;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
 
 /**
- * OrderMessage: Esta clase representa el mensaje que se recibe desde Kafka.
- * Contiene información básica del pedido, como el ID del pedido "orderId", el ID del cliente "customerId", 
- * y una lista de productos asociados al pedido-orden "order". 
- * Es utilizada para la transmisión y procesamiento de pedidos en el sistema.
+ * OrderMessage: This class represents the message received from Kafka.
+ * It contains basic order information such as the order ID ("orderId"),
+ * customer ID ("customerId"), and a list of products associated with the order.
+ * It is used for transmitting and processing orders within the system.
  */
 @Data
 @AllArgsConstructor
