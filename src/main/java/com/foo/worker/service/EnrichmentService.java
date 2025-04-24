114c6035d5a31843884e4bb0a07549d42f97026a
freyder <freyder@hotmail.com>
2025-04-24 10:25:42 -0500

    Update EnrichmentService.java

--- src/main/java/com/foo/worker/service/EnrichmentService.java
+++ src/main/java/com/foo/worker/service/EnrichmentService.java
@@ -7,16 +7,20 @@ import com.foo.worker.models.ProductDetails;
 import reactor.core.publisher.Mono;
 
 /**
- * EnrichmentService: Interfaz que define los métodos para enriquecer los datos de un pedido.
- * tanto cliente "Customer" como los datos del producto"Product"
- * cabe destacar que se utilizo Resilience4J para reintentos y Reactor si se quiere una inspeccion mas granular
+ * EnrichmentService: Interface that defines methods to enrich order data,
+ * including both customer and product information.
+ * 
+ * It is worth mentioning that Resilience4j is used for retries, and Reactor is
+ * used when finer-grained reactive control is needed.
+ * 
  * @author Freyder Otalvaro
  * @version 1.0
  * @since 2024-10-17
  */
 public interface EnrichmentService {
-    
+
     Mono<CustomerDetails> enrichCustomerWithResilience(OrderMessage orderMessage);
+    
     Mono<CustomerDetails> enrichCustomerWithReactor(OrderMessage orderMessage);
     
     Mono<ProductDetails> enrichProductWithResilience(OrderMessage orderMessage);
