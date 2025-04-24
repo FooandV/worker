d5d46c80d3f74e19de9d67b3b3f106c315a4c819
freyder <freyder@hotmail.com>
2025-04-24 10:36:14 -0500

    Update RedisLockService.java

--- src/main/java/com/foo/worker/service/RedisLockService.java
+++ src/main/java/com/foo/worker/service/RedisLockService.java
@@ -3,12 +3,13 @@ package com.foo.worker.service;
 import reactor.core.publisher.Mono;
 
 /**
- * RedisLockService: Interfaz que define los métodos para gestionar locks (bloqueos) distribuidos utilizando Redis.
- * Esta interfaz asegura que solo una instancia del worker pueda procesar un pedido en un momento dado,
- * evitando la concurrencia en el procesamiento de un mismo pedido.
+ * RedisLockService: Interface that defines methods for managing distributed locks using Redis.
+ * This interface ensures that only one instance of the worker can process a specific order at a time,
+ * preventing concurrent processing of the same order.
+ *
+ * Responsibilities:
+ * - Provide methods to acquire and release locks for order processing.
  * 
- * Responsabilidades:
- * - Proveer métodos para adquirir(acquireLock) y liberar(releaseLock) locks para pedidos.
  * @author Freyder Otalvaro
  * @version 1.0
  * @since 2024-10-17
