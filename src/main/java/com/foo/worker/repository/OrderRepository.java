386f2cf96fba69313b93d4d86ecf10cc6927a989
freyder <freyder@hotmail.com>
2025-04-24 10:24:10 -0500

    Update OrderRepository.java

--- src/main/java/com/foo/worker/repository/OrderRepository.java
+++ src/main/java/com/foo/worker/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.foo.worker.repository;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import com.foo.worker.models.Order;
 
-
-public interface OrderRepository extends ReactiveMongoRepository<Order, String>  {
+/**
+ * OrderRepository: Reactive repository interface for managing Order documents
+ * in MongoDB. It extends ReactiveMongoRepository to provide non-blocking
+ * CRUD operations on the "orders" collection.
+ */
+public interface OrderRepository extends ReactiveMongoRepository<Order, String> {
 
 }
